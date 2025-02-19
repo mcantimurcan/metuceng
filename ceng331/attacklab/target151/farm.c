a46ecd145221092932aa9f3c3f3c85b2e8b42efd
Mithat Can Timurcan <108149301+mcantimurcan@users.noreply.github.com>
2025-02-19 15:58:09 +0300

    ceng331 attacklab files

---
+++ ceng331/attacklab/target151/farm.c
@@ -0,0 +1,111 @@
+/* This function marks the start of the farm */
+int start_farm()
+{
+    return 1;
+}
+
+void setval_309(unsigned *p)
+{
+    *p = 3285616968U;
+}
+
+unsigned getval_369()
+{
+    return 3263776991U;
+}
+
+unsigned getval_390()
+{
+    return 2497087816U;
+}
+
+unsigned getval_451()
+{
+    return 3287779656U;
+}
+
+unsigned getval_492()
+{
+    return 2425393224U;
+}
+
+unsigned addval_200(unsigned x)
+{
+    return x + 3352166728U;
+}
+
+void setval_157(unsigned *p)
+{
+    *p = 3285057864U;
+}
+
+void setval_276(unsigned *p)
+{
+    *p = 3599354028U;
+}
+
+void setval_238(unsigned *p)
+{
+    *p = 3354888520U;
+}
+
+unsigned addval_385(unsigned x)
+{
+    return x + 2499250504U;
+}
+
+unsigned addval_321(unsigned x)
+{
+    return x + 3263774872U;
+}
+
+unsigned addval_326(unsigned x)
+{
+    return x + 3281016926U;
+}
+
+void setval_288(unsigned *p)
+{
+    *p = 3277378597U;
+}
+
+unsigned addval_478(unsigned x)
+{
+    return x + 2429978952U;
+}
+
+void setval_348(unsigned *p)
+{
+    *p = 3284306248U;
+}
+
+unsigned getval_101()
+{
+    return 3285057864U;
+}
+
+unsigned addval_330(unsigned x)
+{
+    return x + 2429420872U;
+}
+
+void setval_344(unsigned *p)
+{
+    *p = 3287779656U;
+}
+
+void setval_162(unsigned *p)
+{
+    *p = 2425394264U;
+}
+
+void setval_267(unsigned *p)
+{
+    *p = 3264825365U;
+}
+
+/* This function marks the end of the farm */
+int end_farm()
+{
+    return 1;
+}
